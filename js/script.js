abec8150cbada3c170135d47f1e56a3e3b40fd7f
Jeffrey Ding <jeffreyding18@gmail.com>
2023-11-09 12:54:02 -0800

    Fixed audio

--- js/script.js
+++ js/script.js
@@ -1,49 +1,18 @@
-var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-var audioBuffer;
-var audioSource = null;
-var isPlaying = false;
+document.addEventListener('DOMContentLoaded', (event) => {
+    const videoElement = document.getElementById('videoBG');
+    const audioElement = document.getElementById('audioBG');
 
-function fetchAndDecodeAudio(url) {
-    fetch(url)
-        .then(response => response.arrayBuffer())
-        .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-        .then(decodedAudio => {
-            audioBuffer = decodedAudio;
-        });
-}
+    // Start the audio muted
+    audioElement.muted = true;
+    audioElement.volume = 0.15;
 
-function setupAudioSource() {
-    audioSource = audioContext.createBufferSource();
-    audioSource.buffer = audioBuffer;
-    audioSource.loop = true;
-    audioSource.connect(audioContext.destination);
-}
-
-function toggleAudioPlayback() {
-    if (!isPlaying) {
-        if (!audioSource) {
-            // Setup the audio source if it's the first time playing
-            setupAudioSource();
+    videoElement.addEventListener('click', () => {
+        // Toggle the muted state on click
+        if (audioElement.muted) {
+            audioElement.muted = false;
+            audioElement.play(); // Start playing audio when video is clicked
+        } else {
+            audioElement.muted = true;
         }
-        audioSource.start(0);
-        isPlaying = true;
-    } else {
-        // Stop the current audio source and flag it so a new one will be created next time
-        audioSource.stop();
-        audioSource = null;
-        isPlaying = false;
-    }
-}
-
-// Fetch and decode the audio file right away
-fetchAndDecodeAudio("../assets/audio/mononokesdreams.mp3");
-
-// Event listener for the video click
-document.getElementById('videoBG').addEventListener('click', function() {
-    // Chrome requires audio context to be resumed from a user gesture
-    if (audioContext.state === 'suspended') {
-        audioContext.resume();
-    }
-
-    toggleAudioPlayback();
+    });
 });

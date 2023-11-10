d20f0a82cc0554dbeda4070a68f9921eb876af0d
Jeffrey Ding <jeffreyding18@gmail.com>
2023-11-10 09:32:50 -0800

    Added pausing on click for mobile testing

--- js/script.js
+++ js/script.js
@@ -6,7 +6,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
     audioElement.muted = true;
     audioElement.volume = 0.15;
 
+    function isVideoPlaying(video) {
+        return !!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2);
+    }
+
     videoElement.addEventListener('click', () => {
+        if (isVideoPlaying(videoElement)) {
+            videoElement.pause();
+        } else {
+            videoElement.play();
+        }
+
         // Toggle the muted state on click
         if (audioElement.muted) {
             audioElement.muted = false;
